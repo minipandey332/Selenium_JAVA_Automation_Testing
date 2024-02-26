ac3ffe3d0d6e5884d0c40d6ae987eceadab25dfe
minipandey332 <73335393+minipandey332@users.noreply.github.com>
2024-02-26 11:16:50 +0530

    Day10 Task

---
+++ Day10/src/main/java/com/ncs/Day10/App.java
@@ -0,0 +1,13 @@
+package com.ncs.Day10;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
